b0afaedfbabbda34dadb3af6f700519e2673d2e2
Shahryar Ahmad <shehryarshahid49@gmail.com>
2023-06-17 23:14:19 +0500

    Add files via upload

--- plutonium.cpp
+++ plutonium.cpp
@@ -1,8 +1,7 @@
-
+//#define PLUTONIUM_PROFILE
 #include <signal.h>
 #include <time.h>
 #include <cstdint>
-#include "include/PltObject.h"
 #include "include/plutonium.h"
 
 #define PLUTONIUM_VER 1.5
@@ -12,115 +11,10 @@ void signalHandler(int signum)
   if(signum==SIGABRT || signum==SIGFPE || signum==SIGILL || signum==SIGSEGV)
   {
     fprintf(stderr,"Oops either the interpreter or one of the loaded modules just crashed.Please report this incident.\n");
-    exit(0);
+    exit(1);
   }
-}
-string readline()
-{
-  char ch;
-  string line;
-  while((ch = fgetc(stdin))!='\n')
-  {
-    line+=ch;
-  }
-  return line;
 }
 
-string& readfile(string filename)
-{
-  FILE* fp = fopen(filename.c_str(), "r");
-  static string src;
-  src = "";
-  if (!fp)
-  {
-      printf("Error opening file: %s\n", strerror(errno));
-      exit(0);
-  }
-  signed char ch;
-  while ((ch = fgetc(fp)) != EOF)
-  {
-      if(ch <= 0)
-      {
-        printf("Error the file %s does not seem to be a text file or contains non-ascii characters.\n",filename.c_str());
-        exit(0);
-      }
-      src += ch;
-  }
-  fclose(fp);
-  return src;
-}
-void WriteByteCode(vector<uint8_t>& bytecode,std::unordered_map<size_t,ByteSrc>& LineNumberTable,vector<string>& files)
-{
-
-   FILE* f = fopen("program.pltb","wb");
-   if(!f)
-   {
-    printf("error writing bytecode file \n");
-    return;
-   }
-    //write line number table
-    int total = LineNumberTable.size();
-    fwrite(&total,sizeof(int),1,f);
-
-    for(auto e: LineNumberTable)
-    {
-      size_t Pos = e.first;
-      ByteSrc src = e.second;
-      fwrite(&Pos,sizeof(size_t),1,f);
-      Pos = src.ln;
-      fwrite(&Pos,sizeof(size_t),1,f);
-      string s = files[src.fileIndex];
-      int L  = s.length();
-      fwrite(&L,sizeof(int),1,f);
-      fwrite(s.c_str(),sizeof(char),s.length(),f);
-    }
-    //
-   uint8_t* arr = new uint8_t[bytecode.size()];
-   total = vm.total_constants;
-   fwrite(&total,sizeof(int),1,f);
-   for(int i=0;i<vm.total_constants;i+=1)
-   {
-     string s = PltObjectToStr(vm.constants[i]);
-    // printf("writing constant %s\n",s.c_str());
-     int L = s.length();
-     fwrite(&L,sizeof(int),1,f);
-     fwrite(s.c_str(),sizeof(char),s.length(),f);
-   }
-   int sz = bytecode.size();
-   fwrite(&sz,sizeof(int),1,f);
-    for(size_t k=0;k<bytecode.size();k+=1)
-    {
-      arr[k] = bytecode[k];
-    }
-    fwrite(arr,sizeof(uint8_t),bytecode.size(),f);
-    fclose(f);
-    delete[] arr;
-}
-#ifdef PLUTONIUM_PROFILE
-void showProfileInfo()
-{
-  for(int i=1;i<=66;i+=1)
-  {
-    for(int j=0;j<66;j++)
-    {
-      if(vm.instCount[j] < vm.instCount[j+1])
-      {
-        int c = vm.instCount[j];
-        vm.instCount[j] = vm.instCount[j+1];
-        vm.instCount[j+1] = c;
-        const char* prev = opNames[j];
-        opNames[j] = opNames[j+1];
-        opNames[j+1] = prev;
-      }
-    }
-  }
-  printf("OPCODE  Count\n");
-  for(int i=0;i<67;i++)
-  {
-    printf("%s  %ld\n",opNames[i],vm.instCount[i]);
-  }
-}
-#endif
 void REPL()
 {
   REPL_MODE = true;
@@ -133,7 +27,7 @@ void REPL()
   files.push_back(filename);
   sources.push_back("");
   static std::unordered_map<size_t,ByteSrc> LineNumberTable;
-  static size_t stackSize = 3;
+  static size_t stackSize = 19;
   static Compiler compiler;
   if(vm.STACK.size() > stackSize)
   {
@@ -218,29 +112,14 @@ void REPL()
     sources.push_back("");
   }
 }
-const char* getOS()
-{
-  #ifdef __linux__
-    return "Linux";
-  #elif _WIN64
-    return "Windows 64 bit";
-  #elif _WIN32
-    return "Windows 32 bit";
-  #elif __FreeBSD__
-    return "FreeBSD";
-  #elif __APPLE__ || __MACH__
-    return "Mac OSX";
-  #elif __unix || __unix__
-    return "Unix";
-  #endif
-  return "OS Unknown";
-}
+
 int main(int argc, const char* argv[])
 {
     signal(SIGFPE,signalHandler);
     signal(SIGILL,signalHandler);
     signal(SIGABRT,signalHandler);
     signal(SIGSEGV,signalHandler);
+    
     if (argc < 2)
     {
       vm.constants = new PltObject[1];
@@ -283,7 +162,7 @@ int main(int argc, const char* argv[])
         //vector by the parser
         parser.init(&fnReferenced,&num_of_constants,&files,&sources,filename);
         Node* ast = parser.parse(tokens);
-       // printAST(ast);
+        //printAST(ast);
         Compiler compiler;
         compiler.init(&fnReferenced,&num_of_constants,&files,&sources,&LineNumberTable,filename);
         vm.constants = new PltObject[num_of_constants];
@@ -296,8 +175,6 @@ int main(int argc, const char* argv[])
     }
     //WriteByteCode(bytecode,LineNumberTable,files);
     vm.load(bytecode,&LineNumberTable,&files,&sources);
-    bytecode.clear();
-    bytecode.shrink_to_fit();
     vm.interpret();
     #ifdef PLUTONIUM_PROFILE
      showProfileInfo();
