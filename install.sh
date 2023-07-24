26943650b827c17c6f257b74d38a125eb0810364
Shahryar Ahmad <shehryarshahid49@gmail.com>
2023-07-24 15:11:20 +0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,12 +8,12 @@ g++ plutonium.cpp -o plutonium -ldl -DNDEBUG -O3 -w
 strip plutonium
 cp include/PltObject.h /opt/plutonium/
 cp include/PltObject.h modules/PltObject.h
-g++ -shared modules/math.cpp -o modules/math.so -fPIC -DNDEBUG -O3
-g++ -shared modules/regex.cpp -o modules/regex.so -fPIC -DNDEBUG -O3
-g++ -shared modules/json.cpp -o modules/json.so -fPIC -DNDEBUG -O3
-g++ -shared modules/cgi.cpp -o modules/cgi.so -fPIC -DNDEBUG -O3
-g++ -shared modules/datetime.cpp -o modules/datetime.so -fPIC -DNDEBUG -O3
-g++ -shared modules/mysql.cpp -o modules/mysql.so -fPIC $(mariadb_config --include --libs) -DNDEBUG -O3
+g++ -shared modules/math/math.cpp -o modules/math.so -fPIC -DNDEBUG -O3
+g++ -shared modules/regex/regex.cpp -o modules/regex.so -fPIC -DNDEBUG -O3
+g++ -shared modules/json/json.cpp -o modules/json.so -fPIC -DNDEBUG -O3
+g++ -shared modules/cgi/cgi.cpp -o modules/cgi.so -fPIC -DNDEBUG -O3
+g++ -shared modules/datetime/datetime.cpp -o modules/datetime.so -fPIC -DNDEBUG -O3
+g++ -shared modules/mysql/mysql.cpp -o modules/mysql.so -fPIC $(mariadb_config --include --libs) -DNDEBUG -O3
 g++ tests/test_all.cpp -o tests/test_all
 echo "[+] Running tests"
 cd tests/
