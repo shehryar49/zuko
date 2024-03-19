96b5d5d823bc29cf45fa979733afbbfbc45a88d1
shehryar49 <shehryarshahid49@gmail.com>
2024-03-19 14:23:23 +0500

    Improve installation procedure

--- install.sh
+++ install.sh
@@ -18,6 +18,7 @@ echo "[+] Copying files (might require password)"
 sudo mkdir /opt/zuko
 sudo mkdir /opt/zuko/modules
 sudo mkdir /opt/zuko/include
+sudo mkdir /opt/zuko/lib
 sudo cp zuko /opt/zuko/
 sudo cp -r std /opt/zuko
 sudo cp -r fiza /opt/zuko
@@ -40,6 +41,7 @@ sudo cp include/zfileobj.h /opt/zuko/include
 sudo cp include/zlist.h /opt/zuko/include
 sudo cp include/zobject.h /opt/zuko/include
 sudo cp include/zstr.h /opt/zuko/include
+sudo cp libzapi /opt/zuko/lib
 #
 
 sudo chmod 777 /opt/zuko/fiza/packages.txt
