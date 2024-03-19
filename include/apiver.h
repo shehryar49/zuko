41251435765d50b4c763e85ffbfdb2d57c584865
shehryar49 <shehryarshahid49@gmail.com>
2024-03-19 21:29:52 +0500

    Add apiver.h

---
+++ include/apiver.h
@@ -0,0 +1,10 @@
+#ifndef ZUKO_API_VERSION
+
+//Increment this if there are breaking changes in extension api
+#define ZUKO_API_VERSION 1
+
+//Examples of breaking changes:
+//Some data structure was changed/improved/added
+//Some allocator function was changed
+//Some error klass was changed 
+#endif
