21adb88589ad1a7a5ed3b59acdbfe956ada3d486
shehryar49 <shehryarshahid49@gmail.com>
2024-03-19 13:57:36 +0500

    Fix bug

--- include/klassobject.h
+++ include/klassobject.h
@@ -1,6 +1,9 @@
 #ifndef ZUKO_KLASSOBJ_H_
 #define ZUKO_KLASSOBJ_H_
 
+#ifdef __cplusplus
+extern "C"{
+#endif
 
 struct Klass;
 struct StrMap;
@@ -19,5 +22,10 @@ typedef struct KlassObject
 
 ZObject KlassObj_getMember(KlassObject* ko,const char* name);
 void KlassObj_setMember(KlassObject* ko,const char* name,ZObject val);
+
+#ifdef __cplusplus
+}
+#endif
+
 
 #endif
